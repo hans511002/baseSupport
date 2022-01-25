25a5066b2a1179bbc5fdebba32c7bee47c15b2ef
hans511 <hans511002@sohu.com>
2022-01-25 17:38:09 +0800

    m

--- src/org/apache/log4j/varia/NullAppender.java
+++ src/org/apache/log4j/varia/NullAppender.java
@@ -21,59 +21,60 @@ import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
 
 /**
-  * A NullAppender merely exists, it never outputs a message to any
-  * device.  
-  * @author Ceki G&uuml;lc&uml;
-  */
+ * A NullAppender merely exists, it never outputs a message to any device.
+ * 
+ * @author Ceki G&uuml;lc&uml;
+ */
 public class NullAppender extends AppenderSkeleton {
 
-  private static NullAppender instance = new NullAppender();
+	private static NullAppender instance = new NullAppender();
 
-  public NullAppender() {
-  }
+	public NullAppender() {
+	}
 
-  /** 
-   * There are no options to acticate.
-   * */
-  public void activateOptions() {
-  }
+	/**
+	 * There are no options to acticate.
+	 */
+	public void activateOptions() {
+	}
 
-  /**
-   * Whenever you can, use this method to retreive an instance instead
-   * of instantiating a new one with <code>new</code>.
-   * @deprecated Use getNullAppender instead.  getInstance should have been static.
-   * */
-  public NullAppender getInstance() {
-    return instance;
-  }
+	/**
+	 * Whenever you can, use this method to retreive an instance instead of instantiating a new one with
+	 * <code>new</code>.
+	 * 
+	 * @deprecated Use getNullAppender instead. getInstance should have been static.
+	 */
+	public NullAppender getInstance() {
+		return instance;
+	}
 
-    /**
-     * Whenever you can, use this method to retreive an instance instead
-     * of instantiating a new one with <code>new</code>.
-     * */
-  public static NullAppender getNullAppender() {
-      return instance;
-  }
+	/**
+	 * Whenever you can, use this method to retreive an instance instead of instantiating a new one with
+	 * <code>new</code>.
+	 */
+	public static NullAppender getNullAppender() {
+		return instance;
+	}
 
-  public void close() {
-  }
+	public void close() {
+	}
 
-  /**
-   * Does not do anything. 
-   * */
-  public void doAppend(LoggingEvent event) {
-  }
+	/**
+	 * Does not do anything.
+	 */
+	public void doAppend(LoggingEvent event) {
+	}
 
-  /**
-   * Does not do anything. 
-   * */
-  protected void append(LoggingEvent event) {
-  }
+	/**
+	 * Does not do anything.
+	 */
+	protected void append(LoggingEvent event) {
+	}
 
-  /**
-    * NullAppenders do not need a layout.  
-    * */
-  public boolean requiresLayout() {
-    return false;
-  }
+	/**
+	 * NullAppenders do not need a layout.
+	 */
+	public boolean requiresLayout() {
+		return false;
+	}
 }
